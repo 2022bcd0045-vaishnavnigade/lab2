446b6e13cc6f40f8e04fa84e2936b836886e0c59
2022bcd0045-vaishnavnigade <nigade22bcd45@iiitkottayam.ac.in>
2026-01-11 19:39:25 +0530

    Model-Lasso, alpha-0.1, test_split-0.2, preprocessing-StandardScaler

--- train.py
+++ train.py
@@ -3,39 +3,51 @@ import json
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
 from sklearn.metrics import mean_squared_error, r2_score
 import joblib
 
+
 os.makedirs("output/model", exist_ok=True)
 os.makedirs("output/evaluation", exist_ok=True)
+
 
 data = pd.read_csv("dataset/winequality-red.csv", sep=";")
 
 X = data.drop("quality", axis=1)
 y = data["quality"]
 
+
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
+
 
 X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42
 )
 
-model = LinearRegression()
+
+model = Lasso(alpha=0.1)
 model.fit(X_train, y_train)
 
+
 y_pred = model.predict(X_test)
+
 
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
 
+
 print(f"MSE: {mse}")
 print(f"R2 Score: {r2}")
 
+
 joblib.dump(model, "output/model/model.pkl")
 
+
 results = {
+    "model": "Lasso Regression",
+    "alpha": 0.1,
     "MSE": mse,
     "R2_Score": r2
 }
